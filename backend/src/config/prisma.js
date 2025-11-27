5f4d61f83bdc2044edf317a1089416bd64cb70da
Dev-Soares <bernardo.soares30@outlook.com>
2025-11-27 12:57:19 -0300

    configs de deploy do backend ajustadas

--- backend/src/config/prisma.js
+++ backend/src/config/prisma.js
@@ -1,7 +1,14 @@
-import pkg from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 
-const { PrismaClient } = pkg;
+let prisma;
 
-const prisma = new PrismaClient();
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
+}
 
 export default prisma;
