92d0ebf5d3e5f2366475e7436363091a4c195c0f
Bernardo <bernardo.soares30@outlook.com>
2026-03-30 13:31:25 -0300

    fix: cors fix

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,7 +11,8 @@ const app = express();
 const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:5174',
-  'https://project-builder-frontend.vercel.app'
+  'https://project-builder-frontend.vercel.app',
+  'https://projectbuilder-frontend.vercel.app'
 ];
 
 const corsOptions = {
